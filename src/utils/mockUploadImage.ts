74b3ed28c69238d35ccccc2fb54454c50cd112e1
Ibraim Barrios <barrios.party@hotmail.com>
2025-01-03 21:26:20 -0600

    Remover comnetarios

--- src/utils/mockUploadImage.ts
+++ src/utils/mockUploadImage.ts
@@ -1,28 +1,3 @@
-// export type initialStateType = {
-//     success: boolean;
-//     result: Record<string, string> | null;
-//     error: string | null;
-//   };
-
-// const uploadImage = async (previousState: initialStateType, formData: FormData) => {
-//     const image = formData.get('file');
-
-//     console.log(image, previousState);
-
-//     const result: Record<string, string> = await new Promise((resolve) => {
-//         setTimeout(() => {
-//             resolve({
-//                 url: 'https://via.placeholder.com/150',
-//                 name: 'image-05'
-//             });
-//         }, 2000);
-//     });
-
-//     return { success: true, result, error: null };
-// };
-
-// export default uploadImage;
-
 const uploadImage = async (file: File) => {
   return new Promise((resolve) => {
     setTimeout(() => {
